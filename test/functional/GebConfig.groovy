fcf7d3902be44e455082b219495f978208200264
horii003 <horii@ca2.so-net.ne.jp>
2015-07-02 21:56:13 +0900

    Update GebConfig.groovy

--- test/functional/GebConfig.groovy
+++ test/functional/GebConfig.groovy
@@ -4,7 +4,7 @@ import org.openqa.selenium.Dimension
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.ie.InternetExplorerDriver
-import geb.driver.SauceLabsDriverFactory
+
 import java.io.File
 import org.apache.commons.lang.SystemUtils
 
@@ -41,97 +41,14 @@ driver = {
     d
 }
 
-def sauceDriver = { def browserCaps ->
-    def username = System.getenv("SAUCE_LABS_USER")
-    assert username
-    def accessKey = System.getenv("SAUCE_LABS_ACCESS_PASSWORD")
-    assert accessKey
-    def caps = [:]
-    caps << browserCaps
-    caps.put('name', 'grails-saucelabs-demo')
-    caps.put('build', "git rev-parse HEAD".execute().text)
+def browserStackBrowser = System.getProperty("geb.browserstack.browser")
+if (browserStackBrowser) {
     driver = {
-       new SauceLabsDriverFactory().create(username, accessKey, caps)
+       def username = ""
+       assert username
+       def accessKey = ""
+       assert accessKey
+       new BrowserStackDriverFactory().create(browserStackBrowser, username, accessKey)
     }
 }
 
-// This property comes from the gradle geb-saucelabs plugin
-def sauceLabsBrowser = System.getProperty("geb.saucelabs.browser")
-if (sauceLabsBrowser) {
-  def browserCaps = new Properties()
-  browserCaps.load(new StringReader(sauceLabsBrowser.replaceAll(',','\n')))
-  sauceDriver(browserCaps)
-} else {
-
-  // This comes from the 'geb.env' system property supported by Geb
-  environments {
-    // grails -Dgeb.env=firefox-local test-app functional:
-    'firefox-local' {
-      driver = { new FirefoxDriver() }
-    }
-    // grails -Dgeb.env=chrome-local test-app functional:
-    'chrome-local' {
-      def chromeDriver = new File('target/webdrivers/chrome/chromedriver')
-      if (SystemUtils.IS_OS_WINDOWS) {
-        downloadDriver(chromeDriver, "http://chromedriver.storage.googleapis.com/2.10/chromedriver_win32.zip")
-      } else if (SystemUtils.IS_OS_LINUX
-      ) {
-          if (SystemUtils.OS_ARCH?.
-              contains("64")) {
-            downloadDriver(chromeDriver,
-                "http://chromedriver.storage.googleapis.com/2.10/chromedriver_linux64.zip")
-          } else {
-            downloadDriver(chromeDriver,
-                "http://chromedriver.storage.googleapis.com/2.10/chromedriver_linux32.zip")
-          }
-      } else if (SystemUtils.IS_OS_MAC) {
-        downloadDriver(chromeDriver,
-            "http://chromedriver.storage.googleapis.com/2.10/chromedriver_mac32.zip")
-      }
-      System.setProperty(
-
-          'webdriver.chrome.driver', chromeDriver.absolutePath)
-      driver = { new ChromeDriver() }
-  } // grails -Dgeb.env=firefox test-app functional:
-    'firefox' {
-
-      sauceDriver(browserName: 'firefox', platform:'Windows 7', version: '31') }
-    'firefox-yosemite' {
-
-      sauceDriver(browserName: 'firefox', platform:
-          'OS X 10.10', version:'33') } // grails -Dgeb.env=chrome test-app functional:
-    'chrome' {
-
-      sauceDriver(browserName: 'chrome', platform:
-          'Windows 7', version:'36') } // grails -Dgeb.env=safari test-app functional:
-    'safari' {
-
-      sauceDriver(browserName: 'safari', platform:
-          'OS X 10.9', version:'7') } // grails -Dgeb.env=ie8 test-app functional:
-  'ie8' {
-    sauceDriver(
-
-        browserName: 'internet explorer', platform:
-        'Windows 7', version:'8') } // grails -Dgeb.env=ie9 test-app functional:
-  'ie9' {
-    sauceDriver(
-
-        browserName: 'internet explorer', platform:
-        'Windows 7', version:'9') } // grails -Dgeb.env=ie10 test-app functional:
-  'ie10' {
-    sauceDriver(
-
-        browserName: 'internet explorer', platform:
-        'Windows 7', version:'10') } // grails -Dgeb.env=ie11 test-app functional:
-  'ie11' {
-    sauceDriver(
-        browserName: 'internet explorer', platform: 'Windows 8.1', version:'11') }
-
-  'ios' { sauceDriver(browserName:
-      'iPhone'
-      ,
-
-      platform:'OS X 10.9', version:'7.1', 'device-orientation': 'portrait') }
-}
-
-}
